f1e9767e4610ee44d89f254577956a41bde9993b
kevin <ajiprasetya@live.com>
2022-03-09 00:49:54 +0700

    add route users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const models = require('../models')
+
+const userService = require('../services/users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userService.list)
+router.post('/', userService.add)
 
-module.exports = router;
+module.exports = router
