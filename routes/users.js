f30f39022ddccd3e39a2bb12c6c2285805aafc10
kevin <ajiprasetya@live.com>
2022-03-09 22:37:32 +0700

    add middleware auth

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {body, validationResult, query} = require('express-validator')
+const {body, query} = require('express-validator')
 const db = require('../models')
+const auth = require('../middleware/auth')
 
 const userService = require('../services/users')
 
 router.get('/', userService.list)
 
-router.post('/',
+router.post('/', auth,
     body('username').not().isEmpty().withMessage('username required.')
         .custom(value => {
             return db.Users.findOne({where: {username: value}})
@@ -21,7 +22,7 @@ router.post('/',
     body('name').not().isEmpty().withMessage('name required.'),
     userService.add)
 
-router.put('/',
+router.put('/', auth,
     query('id').not().isEmpty().withMessage('id required'),
     query('username').not().isEmpty().withMessage('username required.')
         .custom(value => {
@@ -34,11 +35,11 @@ router.put('/',
         }).withMessage('username exists.'),
     userService.edit)
 
-router.delete('/',
+router.delete('/', auth,
     query('id').not().isEmpty().withMessage('id required.'),
     userService.deleteData)
 
-router.delete('/permanent',
+router.delete('/permanent', auth,
     query('id').not().isEmpty().withMessage('id required.'),
     userService.deleteDataPermanent)
 
