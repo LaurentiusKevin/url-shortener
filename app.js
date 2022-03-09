92bbdb00db36a035db9d4a6250242e7e850d2e7c
kevin <ajiprasetya@live.com>
2022-03-09 07:53:03 +0700

    add route for short_url and short_url_redirect. Short Url is the CRUD process. Short Url Redirect is where the user will be redirected to original url

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const logger = require('morgan');
 const db = require('./models')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const shortUrlRouter = require('./routes/short_url');
+const shortUrlRedirectRouter = require('./routes/short_url_redirect');
 
 const app = express();
 
@@ -29,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/short-url', shortUrlRouter);
+app.use('/s_*', shortUrlRedirectRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const models = require('../models')
 
 const userService = require('../services/users')
 
